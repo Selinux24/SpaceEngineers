b266980b10bcfd166d4173f22c7fa573f9cf36e7
Selinux <Selinux24@users.noreply.github.com>
2025-09-08 18:10:40 +0200

    Utils project folder created New cargo monitor script

---
+++ Utils/CargoMonitor/Config.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace IngameScript
+{
+    class Config
+    {
+        readonly StringBuilder errors = new StringBuilder();
+      
+        public readonly string CargoContainerName;
+        public readonly double CargoUpperLimit;
+        public readonly double CargoLowerLimit;
+
+        public readonly string TimerOn;
+        public readonly string TimerOff;
+
+        public readonly bool UseAllGrids;
+
+        public Config(string customData)
+        {
+            CargoContainerName = ReadConfig(customData, "CargoContainerName", CargoContainerName);
+            CargoUpperLimit = ReadConfigDouble(customData, "CargoUpperLimit", CargoUpperLimit);
+            CargoLowerLimit = ReadConfigDouble(customData, "CargoLowerLimit", CargoLowerLimit);
+
+            TimerOn = ReadConfig(customData, "TimerOn");
+            TimerOff = ReadConfig(customData, "TimerOff");
+
+            UseAllGrids = ReadConfigBool(customData, "UseAllGrids");
+        }
+        string ReadConfig(string customData, string name, string defaultValue = null)
+        {
+            var value = ReadConfigLine(customData, name);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (defaultValue == null)
+                {
+                    errors.AppendLine($"{name} not set.");
+                }
+
+                return defaultValue;
+            }
+
+            return value;
+        }
+        bool ReadConfigBool(string customData, string name, bool? defaultValue = null)
+        {
+            var value = ReadConfigLine(customData, name);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (!defaultValue.HasValue)
+                {
+                    errors.AppendLine($"{name} not set.");
+                }
+
+                return defaultValue ?? false;
+            }
+
+            return bool.Parse(value.Trim());
+        }
+        double ReadConfigDouble(string customData, string name, double? defaultValue = null)
+        {
+            var value = ReadConfigLine(customData, name);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (!defaultValue.HasValue)
+                {
+                    errors.AppendLine($"{name} not set.");
+                }
+
+                return defaultValue ?? 0;
+            }
+
+            return double.Parse(value.Trim());
+        }
+        static string ReadConfigLine(string customData, string name)
+        {
+            string[] lines = customData.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            string cmdToken = $"{name}=";
+            return lines.FirstOrDefault(l => l.StartsWith(cmdToken))?.Replace(cmdToken, "") ?? "";
+        }
+
+        public bool IsValid()
+        {
+            return errors.Length == 0;
+        }
+        public string GetErrors()
+        {
+            return errors.ToString();
+        }
+
+        public static string GetDefault()
+        {
+            return
+                "CargoContainerName=Cargo\n" +
+                "CargoUpperLimit=0.9\n" +
+                "CargoLowerLimit=0.1\n" +
+                "\n" +
+                "TimerOn=Timer Cargo Above\n" +
+                "TimerOff=Timer Cargo Below\n" +
+                "\n" +
+                "UseAllGrids=false";
+        }
+    }
+}
---
+++ Utils/CargoMonitor/Program.cs
@@ -0,0 +1,189 @@
+﻿using Sandbox.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        const char AttributeSep = '=';
+
+        readonly Config config;
+
+        readonly List<IMyCargoContainer> cargoContainers = new List<IMyCargoContainer>();
+        readonly IMyTimerBlock timerOn;
+        readonly IMyTimerBlock timerOff;
+
+        double lastCapacity = 0;
+
+        public Program()
+        {
+            if (string.IsNullOrWhiteSpace(Me.CustomData))
+            {
+                Me.CustomData = Config.GetDefault();
+
+                Echo("CustomData not set.");
+                return;
+            }
+
+            config = new Config(Me.CustomData);
+            if (!config.IsValid())
+            {
+                Echo(config.GetErrors());
+                return;
+            }
+
+            Echo($"Looking for '{config.CargoContainerName}'");
+            Echo($"Using All Grids: {config.UseAllGrids}");
+
+            cargoContainers = GetBlocksOfType<IMyCargoContainer>(config.CargoContainerName, config.UseAllGrids);
+            if (cargoContainers.Count == 0)
+            {
+                Echo("Cargo Containers Not Found.");
+                return;
+            }
+
+            timerOn = GetBlockWithName<IMyTimerBlock>(config.TimerOn, config.UseAllGrids);
+            if (timerOn == null)
+            {
+                Echo($"Timer '{config.TimerOn}' not found.");
+                return;
+            }
+
+            timerOff = GetBlockWithName<IMyTimerBlock>(config.TimerOff, config.UseAllGrids);
+            if (timerOff == null)
+            {
+                Echo($"Timer '{config.TimerOff}' not found.");
+                return;
+            }
+
+            lastCapacity = CalculateCargoPercentage();
+
+            LoadFromStorage();
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
+
+        public void Save()
+        {
+            SaveToStorage();
+        }
+
+        public void Main(string argument)
+        {
+            ParseTerminalMessage(argument);
+
+            var capacity = CalculateCargoPercentage();
+
+            if (capacity >= config.CargoUpperLimit)
+            {
+                if (lastCapacity < config.CargoUpperLimit)
+                {
+                    Echo($"Capacity {capacity:P1} >= {config.CargoUpperLimit:P1}. Activating '{config.TimerOff}'");
+                    timerOff.StartCountdown();
+                }
+            }
+
+            if (capacity < config.CargoLowerLimit)
+            {
+                if (lastCapacity >= config.CargoLowerLimit)
+                {
+                    Echo($"Capacity {capacity:P1} < {config.CargoLowerLimit:P1}. Activating '{config.TimerOn}'");
+                    timerOn.StartCountdown();
+                }
+            }
+
+            lastCapacity = capacity;
+
+            Echo($"Containers found: {cargoContainers.Count}");
+            Echo($"Current capacity: {capacity:P1}");
+            Echo($"Last capacity: {lastCapacity:P1}");
+        }
+        void ParseTerminalMessage(string argument)
+        {
+            if (argument == "START_BELLOW") lastCapacity = 0;
+            if (argument == "START_ABOVE") lastCapacity = 1;
+        }
+
+        List<T> GetBlocksOfType<T>(string name, bool useAllGrids) where T : class, IMyTerminalBlock
+        {
+            var blocks = new List<T>();
+            if (useAllGrids)
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CustomName.Contains(name));
+            else
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CubeGrid == Me.CubeGrid && b.CustomName.Contains(name));
+            return blocks;
+        }
+        T GetBlockWithName<T>(string name, bool useAllGrids) where T : class, IMyTerminalBlock
+        {
+            var blocks = new List<T>();
+            if (useAllGrids)
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CustomName.Contains(name));
+            else
+                GridTerminalSystem.GetBlocksOfType(blocks, b => b.CubeGrid == Me.CubeGrid && b.CustomName.Contains(name));
+            return blocks.FirstOrDefault();
+        }
+
+        double CalculateCargoPercentage()
+        {
+            if (cargoContainers.Count == 0)
+            {
+                return 0;
+            }
+
+            double max = 0;
+            double curr = 0;
+            foreach (var cargo in cargoContainers)
+            {
+                var inv = cargo.GetInventory();
+                max += (double)inv.MaxVolume;
+                curr += (double)inv.CurrentVolume;
+            }
+
+            return curr / max;
+        }
+
+        void LoadFromStorage()
+        {
+            string[] storageLines = Storage.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            if (storageLines.Length == 0)
+            {
+                return;
+            }
+
+            lastCapacity = ReadDouble(storageLines, "lastCapacity", 0);
+        }
+        void SaveToStorage()
+        {
+            List<string> parts = new List<string>
+            {
+                $"lastCapacity={lastCapacity}",
+            };
+
+            Storage = string.Join(Environment.NewLine, parts);
+        }
+        static string ReadString(string[] lines, string name, string defaultValue = "")
+        {
+            string cmdToken = $"{name}{AttributeSep}";
+            string value = lines.FirstOrDefault(l => l.StartsWith(cmdToken))?.Replace(cmdToken, "") ?? "";
+            if (string.IsNullOrEmpty(value))
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+        static double ReadDouble(string[] lines, string name, double defaultValue = 0)
+        {
+            string value = ReadString(lines, name);
+            if (string.IsNullOrEmpty(value))
+            {
+                return defaultValue;
+            }
+
+            return double.Parse(value);
+        }
+    }
+}
